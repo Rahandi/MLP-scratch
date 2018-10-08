a356a4bd6970fd35d94afad46f62b5d43b4987fa
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2018-10-08 15:02:08 +0700

    relu use numpy indexing

--- activation.py
+++ activation.py
@@ -1,36 +1,34 @@
 from numpy import np
 
+
 def sigmoid(x, derivative=False):
     if derivative:
         return x*(1-x)
     return 1/(1+np.exp(-x))
+
 
 def tanh(x, derivative=False):
     if derivative:
         return 1-(x**2)
     return np.tanh(x)
 
+
 def relu(x, derivative=False):
+    result = x.copy()
     if derivative:
-        for a in range(len(x)):
-            for b in range(len(x[a])):
-                if x[a][b] > 0:
-                    x[a][b] = 1
-                else:
-                    x[a][b] = 0
-        return x
-    for a in range(len(x)):
-        for b in range(len(x[a])):
-            if x[a][b] > 0:
-                pass
-            else:
-                x[a][b] = 0
-    return x
+        result[result > 0] = 1.0
+        result[result < 0] = 0.0
+        return result
+
+    result[result < 0] = 0.0
+    return result
+
 
 def arctan(x, derivative=False):
     if derivative:
         return np.cos(x)**2
     return np.arctan(x)
+
 
 def step(x, derivative=False):
     if derivative:
@@ -49,6 +47,7 @@ def step(x, derivative=False):
                 x[a][b] = 0
     return x
 
+
 def squash(x, derivative=False):
     if derivative:
         for a in range(len(x)):
@@ -62,6 +61,7 @@ def squash(x, derivative=False):
         for b in range(len(x[a])):
             x[a][b] = x[a][b] / (1+abs(x[a][b]))
     return x
+
 
 def gaussian(x, derivative=False):
     if derivative:
