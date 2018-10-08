2ea5d253af9b2d6bac159b5af0c58ad236b356c4
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2018-10-09 01:21:47 +0700

    separate Layer to child class Dense and Input

--- layer.py
+++ layer.py
@@ -1,13 +1,41 @@
 import numpy as np
 import random
 
-class Layer:
-    def __init__(self, layer_type, activation, n_neuron):
-        self.n_neuron = n_neuron
-        self.activation = activation
-    
-    def __call__(self, n_input):
-        self.weight = np.random.randn(n_input,self.n_neuron) * np.sqrt(2.0/n_input)
 
-    def output(self, data_input):
-        return self.activation(np.matmul(data_input, self.weight))
+class Layer:
+    def __init__(self, n_neuron: int):
+        self.n_neuron = n_neuron
+        self.input_data = None
+        self.output_data = None
+        self.error = None
+
+    def __call__(self, n_input: int):
+        pass
+
+    def calc_output(self):
+        pass
+
+
+class Input(Layer):
+    def __init__(self, n_neuron: int):
+        super().__init__(n_neuron)
+        self.trainable = False
+
+    def __call__(self, n_input: int):
+        self.weight = np.eye(n_input, self.n_neuron)
+
+    def calc_output(self):
+        return self.input_data.copy()
+
+
+class Dense(Layer):
+    def __init__(self, n_neuron: int, activation: str):
+        super().__init__(n_neuron)
+        self.trainable = True
+        self.activation = activation
+
+    def __call__(self, n_input: int):
+        self.weight = np.random.randn(n_input, self.n_neuron) * np.sqrt(2.0/n_input)
+
+    def calc_output(self):
+        return self.activation(np.matmul(self.input_data, self.weight))
